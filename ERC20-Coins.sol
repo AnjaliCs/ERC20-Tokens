aebca6bd338b83d0572901ae2c517322cdbe9e32
AnjaliCs <68841517+AnjaliCs@users.noreply.github.com>
2023-05-24 16:46:49 +0530

    Create ERC20-Coins.sol
    Mint and Burn

---
+++ ERC20-Coins.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier : MIT;
+pragma solidity^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Token is ERC20, Ownable {
+    constructor() ERC20("Dept Tokens", "DPT") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
